8f5dff49fea25e052611143b74027c9ec3f55401
adharamonzon <adharamonzon@gmail.com>
2020-03-19 19:24:46 +0100

    add structure to html, and styles to css for better read, add fetch in js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const bnt = document.querySelector('.js-btn');
+const list = document.querySelector('.js-list');
+
+let series = [];
+
+function getSerie() {
+  const newInput = document.querySelector('.js-input').value;
+
+  fetch(`http://api.tvmaze.com/shows?q=${newInput}`)
+    .then(response => response.json())
+    .then(data => {
+      series = data;
+    });
+  paintCard();
+}
+
+bnt.addEventListener('click', getSerie);
+
+function paintCard() {
+  let listCode = '';
+  listCode += `<li class="js-li">`;
+  listCode += `<h3 class="serie-title">nombre de la serie </h3>`; //${input.name}
+  listCode += `<img class"image" src="" title="serie (nombre de la serie" alt="fotografía de la serie: (nombre de la serie")">`; //${input.src}
+  listCode += `<p>descripción de la serie </p>`;
+  listCode += `</li>`;
+  list.innerHTML = listCode;
+}
+getSerie();
+paintCard();
