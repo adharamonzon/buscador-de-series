52d54c28ddc1f43cb8ba5b080ca2c6125719c01b
adharamonzon <adharamonzon@gmail.com>
2020-03-20 14:28:48 +0100

    solve problems with the iteration of the data array in the fetch function

--- js/main.js
+++ js/main.js
@@ -2,34 +2,38 @@
 
 const bnt = document.querySelector('.js-btn');
 const list = document.querySelector('.js-list');
+let favoritesList = document.querySelector('.js-favorite-list');
 
 let series = [];
+let favorites = [];
 
-function getSerie() {
+function getSerie(ev) {
+  ev.preventDefalut;
   const newInput = document.querySelector('.js-input').value;
-
-  fetch(`http://api.tvmaze.com/shows?q=${newInput}`)
+  fetch(`http://api.tvmaze.com/search/shows?q=${newInput}`)
     .then(response => response.json())
     .then(data => {
-      series = data;
+      for (let i = 0; i < data.length; i++) {
+        series.push({ id: data[i].show.id, name: data[i].show.name, image: data[i].show.image.medium });
+        if (series.image === null) {
+          serie.image('https://via.placeholder.com/210x295/ffffff/666666/? text=TV');
+        }
+      }
     });
-  for (const serie of series) {
-    serie.value = series;
-  }
-
-  /*   paintCard(); */
+  favorites.push(series[0]);
+  paintCard();
 }
-console.log(series);
+
 bnt.addEventListener('click', getSerie);
 
 function paintCard() {
   let listCode = '';
-  listCode += `<li class="js-li">`;
-  listCode += `<h3 class="serie-title">nombre de la serie </h3>`; //${input.name}
-  listCode += `<img class"image" src="" title="serie (nombre de la serie" alt="fotografía de la serie: (nombre de la serie")">`; //${input.src}
-  listCode += `<p>descripción de la serie </p>`;
-  listCode += `</li>`;
-  list.innerHTML = listCode;
+  for (const serie of series) {
+    listCode += `<li class="list-item" id="${serie.id}">`;
+    listCode += `<h3 class="serie-title">${serie.name} </h3>`;
+    listCode += `<div class="image-container"><img class"image" src="${serie.image}" title="serie ${serie.name}" alt="fotografía de la serie: ${serie.name})</div>">`;
+    listCode += `</li>`;
+    list.innerHTML = listCode;
+  }
+  favoritesList.innerHTML = listCode;
 }
-getSerie();
-/* paintCard(); */
